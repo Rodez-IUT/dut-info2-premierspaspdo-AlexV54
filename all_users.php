fe6a88a1bd3e35c72dba4fc0df4939bc78c07e92
Alex <alexandre.verge@iut-rodez.fr>
2019-11-19 23:57:24 +0100

    fix #1 Base de données prête

---
+++ all_users.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Accueil</title>
+	</head>
+	
+	<body>
+	<h1>All users</h1>
+	<table>
+		<tr>
+			<td>Id</td>
+			<td>Username</td>
+			<td>Email</td>
+			<td>Status</td>
+		</tr>
+	</table>
+		
+	</body>
+</html>
